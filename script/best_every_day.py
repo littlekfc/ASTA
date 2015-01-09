e8185137a94cfd1cd26aa7b80bed424e72102644
littlekfc <309999096@qq.com>
2015-01-09 21:53:59 +0800

    [best every day] improve

--- script/best_every_day.py
+++ script/best_every_day.py
@@ -11,6 +11,7 @@ class BestEveryDay(object):
     BOLL_TECH_ANAL = "http://ifzq.gtimg.cn/appstock/indicators/BOLL/D1?market=SZ&code=%s&args=20&start=&end=&limit=320&fq=qfq"
     #http://baike.baidu.com/view/965844.htm
     SZ_A = "000%03d"
+    TIMEOUT = 10
 
 
     def __init__(self):
@@ -23,14 +24,16 @@ class BestEveryDay(object):
             stock_price_url = self.STOCK_PRICE_URL % stock_num
             boll_url = self.BOLL_TECH_ANAL % stock_num
             try:
-                stock_info = requests.get(stock_price_url).json()
-                boll_info = requests.get(boll_url).json()
+                stock_info = requests.get(stock_price_url, timeout=self.TIMEOUT).json()
+                boll_info = requests.get(boll_url, timeout=self.TIMEOUT).json()
                 last_day_boll = boll_info['data'][-2]
                 last_day_price = stock_info['data']['sz%s' % stock_num]['qfqday'][-2]
                 name = stock_info['data']['sz%s' % stock_num]['qt']['sz%s' % stock_num][1]
-                logging.info('start -- %s' % name)
-                if (last_day_price[2] - last_day_boll['LB']) / last_day_price[2] <= 0.05:
-                    logging.info('get -- (%s, %s)' % (name, stock_num))
+                logging.info('start -- %s, %s, %s, %s, %s' % (name, stock_num, last_day_price[2], last_day_boll['LB'], \
+                                        (float(last_day_price[2]) - float(last_day_boll['LB'])) / (float(last_day_price[2])*1.0)
+                                                             ))
+                if (float(last_day_price[2]) - float(last_day_boll['LB'])) / (float(last_day_price[2])*1.0) <= 0.02:
+                    logging.info('get -- (%s, %s, price: %s, (price-boll)/price: %s )' % (name, stock_num, last_day_price[2], (float(last_day_price[2]) - float(last_day_boll['LB'])) / (float(last_day_price[2])*1.0)))
             except Exception as e:
                 pass
 
