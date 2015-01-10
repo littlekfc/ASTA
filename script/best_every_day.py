5cb08acbc1250840928508e83c865b8eed232129
littlekfc <309999096@qq.com>
2015-01-10 16:31:00 +0800

    [script -- best every data] add template

--- script/best_every_day.py
+++ script/best_every_day.py
@@ -4,6 +4,8 @@
 import requests
 import json
 import logging
+import datetime
+import tornado.template as template
 
 class BestEveryDay(object):
 
@@ -36,6 +38,10 @@ class BestEveryDay(object):
     def __init__(self):
         logging.basicConfig(filename='best.log', format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
         logging.getLogger('requests').setLevel(logging.ERROR)
+        date = "%s" % datetime.datetime.today().date() 
+        self.json_file = file("../data/stock_every_day/%s.json" % date, "w")
+        self.html_file = file("../data/stock_every_day/%s.html" % date, "w")
+        self.best = []
 
     def find_best(self, market):
         for i in xrange(1, 1000):
@@ -47,13 +53,19 @@ class BestEveryDay(object):
                 stock_info = requests.get(stock_price_url, timeout=self.TIMEOUT).json()
                 boll_info = requests.get(boll_url, timeout=self.TIMEOUT).json()
                 last_day_boll = boll_info['data'][-2]
-                last_day_price = stock_info['data'][full_code]['qfqday'][-2]
+                last_day_price = stock_info['data'][full_code]['qt'][full_code][3]
                 name = stock_info['data'][full_code]['qt'][full_code][1]
-                rate = (float(last_day_price[2]) - float(last_day_boll['LB'])) / float(last_day_price[2])
-                logging.info('start -- %s, %s, %s, %s, %s' % (name, stock_num, last_day_price[2],\
+                rate = (float(last_day_price) - float(last_day_boll['LB'])) / float(last_day_price)
+                logging.info('start -- %s, %s, %s, %s, %s' % (name, stock_num, last_day_price,\
                                                               last_day_boll['LB'], rate))
                 if rate <= self.LOW_RATE and name.find("ST") == -1 and name.find(u"退市") == -1:
-                    logging.info('get -- (%s, %s, price: %s, rate: %s )' % (name, stock_num, last_day_price[2], rate))
+                    logging.info('get -- (%s, %s, price: %s, rate: %s )' % (name, stock_num, last_day_price, rate))
+                    self.best.append({
+                        "name" : name, 
+                        "stock_code" : stock_num,
+                        "current_price" : last_day_price,
+                        "price_to_low_boll_rate": rate
+                    })
             except Exception as e:
                 logging.info("error %s" % e)
                 pass
@@ -63,6 +75,11 @@ class BestEveryDay(object):
         self.find_best(self.SH_A1)
         self.find_best(self.SH_A2)
         self.find_best(self.SH_A3)
+        self.json_file.write(json.dumps(self.best))
+        self.json_file.close()
+        loader = template.Loader("../template")
+        self.html_file.write(loader.load("success_every_day.html").generate(data=self.best))
+        self.html_file.close()
 
 
 if __name__ == "__main__":
